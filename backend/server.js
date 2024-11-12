6da019ec76d6a05f2527c7dfbb1fd102e4febd19
Biplab Naskar <bnaskar851@gmail.com>
2024-11-12 11:19:15 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,39 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
 import { connectDB } from "./config/db.js";
 import playerRouter from "./routes/playerRoute.js";
 import path from "path";
-import { fileURLToPath } from "url";
+ 
 
-// Set up __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-// App config
+//app config
+
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Middleware
-app.use(express.json());
-app.use(cors());
 
-// Database connection
+// middleware
+app.use(express.json())
+app.use(cors())
+
+
+// db connection
 connectDB();
 
-// API endpoints
-app.use("/api/player", playerRouter);
-
-// Serve static images from 'uploads' directory
+//api endpoints
+app.use("/api/player",playerRouter)
 app.use("/images", express.static(path.join(__dirname, "uploads")));
 
-// Root route
-app.get("/", (req, res) => {
-    res.send("API working");
-});
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-});
+app.get("/",(req,res)=>{
+    res.send("API working")
+})
+
+app.listen(port,()=>{
+    console.log(`server started on http://localhost:${port}`)
+})
+
+
+
+
+
